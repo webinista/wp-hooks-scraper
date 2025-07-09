d56d78d5ffcd149d996389b26d0b994480fa9737
Tiffany Brown <tiffany@webinista.com>
2025-07-09 16:33:39 -0700

    Add a Conf::SINCE_URL constant.

--- classes/Conf.php
+++ classes/Conf.php
@@ -1,8 +1,9 @@
 <?php namespace WPScrape;
 
 final class Conf {
+  const WP_VERSION  = '6.8.0';
   const HOOKS_URL_BASE = 'https://developer.wordpress.org/reference/hooks/%s/';
   const FILTERS_URL = 'https://developer.wordpress.org/apis/hooks/filter-reference/';
   const ACTIONS_URL = 'https://developer.wordpress.org/apis/hooks/action-reference/';
-  const WP_VERSION  = '6.8';
+  const SINCE_URL = 'https://developer.wordpress.org/reference/since/%s/?post_type[]=wp-parser-hook';
 }
